5b277f7ebacdf807656f9a23de5009771d04cc89
Siddharth Anikar <siddharth.anikar@dal.ca>
2019-12-03 00:40:32 -0400

    ProductPage

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -16,10 +18,37 @@ import com.example.myapplication.R;
 
 public class HomeFragment extends Fragment {
     public View view;
+    ViewFlipper vf;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        int images[] = {R.drawable.slide1,R.drawable.slide2};
+
+        vf = view.findViewById(R.id.flipper);
+
+        for (int image : images)
+        {
+            flipper(image);
+        }
+
         return view;
+    }
+
+    public void flipper(int image)
+    {
+        ImageView imageView = new ImageView(getActivity().getApplicationContext());
+        imageView.setBackgroundResource(image);
+
+        vf.addView(imageView);
+        vf.setFlipInterval(4000);
+        vf.setAutoStart(true);
+
+        //Animation
+
+        vf.setInAnimation(getActivity().getApplicationContext(),android.R.anim.slide_in_left);
+        vf.setOutAnimation(getActivity().getApplicationContext(),android.R.anim.slide_out_right);
+
     }
 
     }
